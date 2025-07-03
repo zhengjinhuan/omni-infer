965c72460d989f4e7ed54cfbee6f1faae3b71813
Yao Yunxiang <14836116+yyaoaj@user.noreply.gitee.com>
2025-07-03 20:56:43 +0800

    add patch to support return prefilled token directly and skip tokenize in decoder instances

--- infer_engines/bash_install_code.sh
+++ infer_engines/bash_install_code.sh
@@ -4,7 +4,7 @@ set -e
 PATCH_ROOT=../../omni/adaptors/vllm/patches/
 
 cd ./vllm
-git checkout tags/v0.9.0
+# git checkout tags/v0.9.0
 git apply $PATCH_ROOT/null_value_handling.patch
 git apply --whitespace=nowarn $PATCH_ROOT/manual_apiserver_scaleout.patch
 git apply --whitespace=nowarn $PATCH_ROOT/scheduler_kv_cache_manager_partial_kv_transfer.patch
@@ -16,4 +16,4 @@ git apply --whitespace=nowarn $PATCH_ROOT/vllm_all_to_all.patch
 # git apply --whitespace=nowarn $PATCH_ROOT/async_schedule_update_output.patch
 git apply --whitespace=nowarn $PATCH_ROOT/bug_fix.patch
 git apply --whitespace=nowarn $PATCH_ROOT/mtp.patch
-# git apply --whitespace=nowarn $PATCH_ROOT/patch_support_trans_prefilled_token_to_decode_instance.patch
+git apply --whitespace=nowarn $PATCH_ROOT/patch_support_trans_prefilled_token_for_chat.patch
