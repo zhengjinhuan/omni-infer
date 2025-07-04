14aafad132c525ff5862bc17557a0b8e2d888a40
He Jian <hejian11@huawei.com>
2025-07-04 10:26:30 +0800

    清理当前没有用到的测试脚本

--- tests/start_vllm_server/deepseek_dp8_tp1_ep1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
-set -e
-
-MODEL_PATH=$1
-vllm serve ${MODEL_PATH} --served-model-name=deepseek --trust-remote-code --max-model-len=8192 --gpu-memory-utilization=0.95 --data-parallel-size 2 --data-parallel-size-local 2  -tp=4 --enable-expert-parallel --block_size 128
