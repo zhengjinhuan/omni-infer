5db331d359c4f3dfa3f136603cba2faea13c850b
Yao Yunxiang <14836116+yyaoaj@user.noreply.gitee.com>
2025-07-05 14:59:36 +0800

    resolve the confilct with wangjun's merges

--- infer_engines/bash_install_code.sh
+++ infer_engines/bash_install_code.sh
@@ -17,3 +17,4 @@ git apply --whitespace=nowarn $PATCH_ROOT/vllm_all_to_all.patch
 git apply --whitespace=nowarn $PATCH_ROOT/bug_fix.patch
 git apply --whitespace=nowarn $PATCH_ROOT/mtp.patch
 git apply --whitespace=nowarn $PATCH_ROOT/patch_support_trans_prefilled_token_for_chat.patch
+git apply --whitespace=nowarn $PATCH_ROOT/num_token_for_proxy.patch
