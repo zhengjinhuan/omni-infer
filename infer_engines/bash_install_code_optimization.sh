ac37e5c48204fad8a98af3f7d8cd201eaa21d0d3
Daylight <xutianliang1@huawei.com>
2025-07-08 20:27:13 +0800

    fix syntax error

--- infer_engines/bash_install_code_optimization.sh
+++ infer_engines/bash_install_code_optimization.sh
@@ -9,7 +9,5 @@ git apply $PATCH_ROOT/null_value_handling.patch
 git apply --whitespace=nowarn $PATCH_ROOT/tokenizer_proc_pool.patch
 git apply --whitespace=nowarn $PATCH_ROOT/manual_apiserver_scaleout.patch
 git apply --whitespace=nowarn $PATCH_ROOT/scheduler_kv_cache_manager_partial_kv_transfer.patch
-git apply --whitespace=nowarn $PATCH_ROOT/all_reduce_optimization.patch
 # git apply --whitespace=nowarn $PATCH_ROOT/async_schedule_update_output.patch
-git apply --whitespace=nowarn $PATCH_ROOT/bug_fix.patch
 git apply --whitespace=nowarn $PATCH_ROOT/chunked_prefill_disable.patch
