28e3f33f17e772d3a62ab722603ada6e4496f658
吴航 <wuhang6@huawei.com>
2025-07-14 09:16:30 +0800

    install patch for manipulate api server keepalive timeout

--- infer_engines/bash_install_code.sh
+++ infer_engines/bash_install_code.sh
@@ -9,3 +9,4 @@ git apply --whitespace=nowarn $PATCH_ROOT/manual_apiserver_scaleout.patch
 git apply --whitespace=nowarn $PATCH_ROOT/scheduler_kv_cache_manager_partial_kv_transfer_v0.9.0.patch
 git apply --whitespace=nowarn $PATCH_ROOT/tokenizer_proc_pool_v0.9.0.patch
 git apply --whitespace=nowarn $PATCH_ROOT/mtp.patch
+git apply --whitespace=nowarn $PATCH_ROOT/api_server_keepalive_timeout.patch
