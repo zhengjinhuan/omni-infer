4f3150aeb6cc6c7d7e358c3d1ce54eaef0525be6
Nicooo Wang <wangtianle3@huawei.com>
2025-07-21 08:55:15 +0800

    remove vllm ascend

--- omni/adaptors/vllm/worker/npu_schedule.py
+++ omni/adaptors/vllm/worker/npu_schedule.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# This file is a part of the vllm-ascend project.
 #
 from collections import deque
 from dataclasses import dataclass, fields
