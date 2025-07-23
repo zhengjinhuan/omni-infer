e751d48d93a3ba8e2ed5513f2c1aa85c33330579
kerryyang <15255597+kerryyang@user.noreply.gitee.com>
2025-07-23 06:54:55 +0000

    update omni/adaptors/vllm/patches/profiler_patches/tfas_profiler.py.
    Signed-off-by: kerryyang <15255597+kerryyang@user.noreply.gitee.com>

--- omni/adaptors/vllm/patches/profiler_patches/tfas_profiler.py
+++ omni/adaptors/vllm/patches/profiler_patches/tfas_profiler.py
@@ -118,43 +118,36 @@ def IQR_filter(df):
     return result 
 
 def profiler_1K_fixed(log_file_path, output_path):
+    pattern1=r"current num reqs = (\d+), num_input_tokens = (\d+)"
+    pattern2=r"execute model cost:([\d.]+)=([\d.+]+)"
     records_running_seqs = []
     records_execute_time = []
     records_tokens = []
-    records_waiting_reqs = []
-
+    
     try:
         with open(log_file_path, "r", encoding="utf-8") as file:
             for line in file:
-                if "execute time" in line:
-                    s = [_ for _ in line.split("|") if "seqs:" in _]
-                    e = [_ for _ in line.split("|") if "execute time:" in _]
-                    t = [_ for _ in line.split("|") if "tokens:" in _]
-                    w = [_ for _ in line.split("|") if "waiting_reqs_num_after_step=" in _][-1:]
-                    records_running_seqs += s
-                    records_execute_time += e
-                    records_tokens += t
-                    records_waiting_reqs += w
+                match1=re.findall(pattern1, line)
+                match2=re.findall(pattern2, line)
+                if match1:
+                    records_running_seqs.append(int(match1[0][0]))
+                    records_tokens.append(int(match1[0][1]))
+                elif match2:
+                    records_execute_time.append(float(match2[0][1].split('+')[1]))
+                else:
+                    pass
     except FileNotFoundError as e:
-        raise ValueError({e}) 
+        raise ValueError({e})
     except Exception as e:
         raise ValueError({e})
-
-    running_seqs = np.array([int(e.split(":")[-1]) for e in records_running_seqs])
-    execute_time = np.array([float(e.split(":")[-1]) for e in records_execute_time])
-    tokens = np.array([float(e.split(":")[-1]) for e in records_tokens])
-    waiting_reqs = np.array([float(e.split("=")[-1]) for e in records_waiting_reqs])
-
+    
     # 获取profiler数据
     df = pd.DataFrame()
-    df["seqs"] = running_seqs
-    df["etime"] = execute_time / 1000
-    df["tokens"] = tokens
-    df["wq"] = waiting_reqs
-    df = df[df.seqs>0] 
+    df["seqs"] = records_running_seqs
+    df["etime"] = records_execute_time
+    df["tokens"] = records_tokens
+    df = df[df.seqs>0]
     df["input_len"] = df["tokens"] / df["seqs"]
-
-    
  
     # 获取prefiler统计结果
     df_sel = df[df.input_len == 1025]
