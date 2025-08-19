ae1de16cfb23e6939988495674a9b6575519a9b5
Yao Yunxiang <yao.yunxiang@huawei.com>
2025-08-19 19:28:35 +0800

    add support for vllm-v0.8.3 on omniinfer-v0.3.0

--- infer_engines/bash_install_code_v0.8.3.sh
+++ infer_engines/bash_install_code_v0.8.3.sh
@@ -9,6 +9,8 @@ git reset --hard
 git clean -fd
 git checkout tags/v0.8.3
 git apply --whitespace=nowarn $PATCH_ROOT/v0.8.3/support_omni_infer_for_v0.8.3.patch
-git apply --whitespace=nowarn $PATCH_ROOT/freeze_numpy.patch
+git apply --whitespace=nowarn $PATCH_ROOT/v0.8.3/solve_triton_issue_on_a_plus_x.patch
+git apply --whitespace=nowarn $PATCH_ROOT/v0.8.3/adapt_vllm_to_omniinfer_v0.3.0.patch
+git apply --whitespace=nowarn $PATCH_ROOT/v0.8.3/common_dependency.patch
 
-SETUPTOOLS_SCM_PRETEND_VERSION=0.8.3 VLLM_TARGET_DEVICE=empty pip install -e .
+# SETUPTOOLS_SCM_PRETEND_VERSION=0.8.3 VLLM_TARGET_DEVICE=empty pip install -e .
