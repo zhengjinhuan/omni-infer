f978b9d5f02d3fd3617c7fa276f461ab0a04c515
wangrui-hw <wangrui235@huawei.com>
2025-08-26 17:44:59 +0800

    add stream usage patch ,how to turn on :  'stream': true, 'stream_options':{'include_usage': true,'continuous_usage_stats': true}

--- infer_engines/bash_install_sglang.sh
+++ infer_engines/bash_install_sglang.sh
@@ -9,3 +9,4 @@ git clean -fd
 git checkout v0.5.0rc0
 git apply --whitespace=nowarn $PATCH_ROOT/npu_support.patch
 git apply --whitespace=nowarn $PATCH_ROOT/triton_placeholder.patch
+git apply --whitesapce=nowarn $PATCH_ROOT/add_stream_usage.patch
