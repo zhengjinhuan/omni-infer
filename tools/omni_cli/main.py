d4b4f230d9f2d607011c8f6f35e4ca30ef174b8d
chengda_wu <wuchengda@huawei.com>
2025-08-27 06:27:35 +0000

    run_docker and start use the same argument,  config_path
    Signed-off-by: chengda_wu <wuchengda@huawei.com>

--- tools/omni_cli/main.py
+++ tools/omni_cli/main.py
@@ -1116,7 +1116,7 @@ def main():
     # RUN_DOCKER command configuration
     run_docker_parser = subparsers.add_parser("run_docker", help="Run Docker containers based on inventory")
     run_docker_parser.add_argument(
-        "--inventory", "-i",
+        "--config_path", "-i",
         default=str(default_deploy_path),
         help=f"Path to server_profiles.yml (default: {default_deploy_path})"
     )
@@ -1168,18 +1168,23 @@ def main():
 
     args = parser.parse_args()
     
-    if args.config_path is None and args.normal is None:                                       
-        args.deploy_path = get_default_deploy_path(args.command)                               
-        default_deploy_path = args.deploy_path                                                 
-    else:                                                                                      
-        if args.config_path is None:                                                           
-            default_deploy_path = args.normal                                                  
-        if args.normal is None:                                                                
-            default_deploy_path = args.config_path    
-    
     if hasattr(args, 'func'):
-        args.func(args)
-        return
+        if args.config_path is None:                                                           
+            args.deploy_path = get_default_deploy_path(args.command)                           
+            default_deploy_path = args.deploy_path                                             
+        else:                                                                                  
+            default_deploy_path = args.config_path                                             
+            args.func(args)
+            return
+    else:                                                                                      
+        if args.config_path is None and args.normal is None:                                   
+            args.deploy_path = get_default_deploy_path(args.command)                           
+            default_deploy_path = args.deploy_path                                             
+        else:                                                                                  
+            if args.config_path is None:                                                       
+                default_deploy_path = args.normal                                              
+            if args.normal is None:                                                            
+                default_deploy_path = args.config_path  
 
     if args.command == "start" and not any([args.normal, args.run_dev]):
         args.normal = True
