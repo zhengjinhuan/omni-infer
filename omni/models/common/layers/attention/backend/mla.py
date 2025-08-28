ce1f570453846a5c3112f8db4c15d2c4a99d9825
DINGYiBin <dybmath@yeah.net>
2025-08-28 16:06:13 +0800

    ar

--- omni/models/common/layers/attention/backend/mla.py
+++ omni/models/common/layers/attention/backend/mla.py
@@ -194,6 +194,16 @@ class AscendMLAMetadata:
     def __post_init__(self):
         pass
 
+    @staticmethod
+    def advance_step(metadata, positions, block_size, pad_mask):
+        block_table = metadata.decode.block_table
+        block_indices = block_table.gather(dim=1, index=(positions // block_size).reshape(-1, 1))
+        block_offsets = positions % block_size
+        metadata.slot_mapping[:] = torch.where(
+            pad_mask,
+            metadata.slot_mapping,
+            block_indices * block_size + block_offsets)
+        metadata.decode.seq_lens[:] = (positions + 1).to(metadata.decode.seq_lens.dtype)
 
 M = TypeVar("M", bound=AscendMLAMetadata)
 
