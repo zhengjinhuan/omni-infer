739f7284ae2426bb8a9b90fbcd5fad84e46edca3
dingyibin <dingyibin1@huawei.com>
2025-09-16 10:26:02 +0800

    get it!

--- tools/speculative_train/merge_files.py
+++ tools/speculative_train/merge_files.py
@@ -10,7 +10,7 @@ def main(input_dir, output_dir):
         filepath = os.path.join(input_dir, file)
         data.extend(torch.load(filepath))
 
-    torch.save(data, os.path.join(output_dir, f"merged-tokens-{time.time_ns()}.pt"))
+    torch.save(data, os.path.join(output_dir, f"merged-{time.time_ns()}.pt"))
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
