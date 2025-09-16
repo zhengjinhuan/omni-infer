51d910cc542a31f6b9b58161b4e2fb0e42612c7a
dingyibin <dingyibin1@huawei.com>
2025-09-16 11:20:46 +0800

    merge

--- tools/speculative_train/merge_files.py
+++ tools/speculative_train/merge_files.py
@@ -3,14 +3,14 @@ import sys
 import time
 import torch
 
-def main(input_dir, output_dir):
+def main(input_dir, output_file):
     files = os.listdir(input_dir)
     data = []
     for file in files:
         filepath = os.path.join(input_dir, file)
         data.extend(torch.load(filepath))
 
-    torch.save(data, os.path.join(output_dir, f"merged-{time.time_ns()}.pt"))
+    torch.save(data, output_file)
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
