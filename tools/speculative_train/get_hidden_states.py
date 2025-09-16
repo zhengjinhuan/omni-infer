6574c47eca22e833bf7558c396f194cbcaea49f5
dingyibin <dingyibin1@huawei.com>
2025-09-16 10:48:13 +0800

    tyr

--- tools/speculative_train/get_hidden_states.py
+++ tools/speculative_train/get_hidden_states.py
@@ -6,27 +6,32 @@ import sys
 import time
 import torch
 
-def get_call_function(ip, port, model_name):
-    url = f"http://{ip}:{port}/v1/completions"
-    headers = {"Content-Type": "application/json"}
-    def make_api_call(input):
-        data = {
-            "model": model_name,
-            "temperature": 0.6,
-            "max_tokens": 2,
-            "include_stop_str_in_output": True,
-            "prompt": input["prompt_token_ids"] + input['output_token_ids']
-        }
-        response = requests.post(url, headers=headers, data=json.dumps(data))
+url = ""
+headers = {}
+model_id = ""
 
-        return response.status_code == 200
-    return make_api_call
+def make_api_call(input):
+    data = {
+        "model": model_id,
+        "temperature": 0.6,
+        "max_tokens": 2,
+        "include_stop_str_in_output": True,
+        "prompt": input["prompt_token_ids"] + input['output_token_ids']
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    return response.status_code == 200
 
 def main(ip, port, model_name, n_num, datafile):
-    call_function = get_call_function(ip, port, model_name)
+    global url
+    url = f"http://{ip}:{port}/v1/completions"
+    global headers
+    headers = {"Content-Type": "application/json"}
+    global model_id
+    model_id = model_name
     data = torch.load(datafile)
     with Pool(n_num) as pool:
-        results = pool.map(call_function, data)
+        results = pool.map(make_api_call, data)
     return results
 
 if __name__ == "__main__":
@@ -39,5 +44,5 @@ if __name__ == "__main__":
     results = main(ip, port, model_name, n_num, datafile)
 
     print(f"succeed case : {sum(results)} / {len(results)}")
-    
+
 
