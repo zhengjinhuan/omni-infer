bad8f9f8c578ca15154ebdf5faffcdcad8c69d33
He Jian <hejian11@huawei.com>
2025-09-16 15:31:16 +0000

    remove omniplacement and mtp patch temporarily
    Signed-off-by: He Jian <hejian11@huawei.com>

--- infer_engines/bash_install_sglang.sh
+++ infer_engines/bash_install_sglang.sh
@@ -12,5 +12,3 @@ git apply --whitespace=nowarn $PATCH_ROOT/triton_placeholder.patch
 git apply --whitespace=nowarn $PATCH_ROOT/add_stream_usage.patch
 git apply --whitespace=nowarn $PATCH_ROOT/add_multi_api_server_deployment.patch
 git apply --whitespace=nowarn $PATCH_ROOT/operator_change_to_improve_profiler.patch
-git apply --whitespace=nowarn $PATCH_ROOT/omni_placement.patch
-git apply --whitespace=nowarn $PATCH_ROOT/log_model_forward_time_cost.patch
