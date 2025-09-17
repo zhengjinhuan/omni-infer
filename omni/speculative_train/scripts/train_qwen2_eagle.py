0202e5ec8bd7304a8cd936fefe8f320e89a3bb5b
dingyibin <dingyibin1@huawei.com>
2025-09-17 17:27:08 +0800

    1727

--- omni/speculative_train/scripts/train_qwen2_eagle.py
+++ omni/speculative_train/scripts/train_qwen2_eagle.py
@@ -5,3 +5,8 @@ model = AutoEagleDraftModel.from_config(config).npu()
 print(model)
 names = [item[0] for item in model.named_parameters()]
 print(names)
+
+model = AutoEagleDraftModel.from_pretrained("/data/model/qwq-32b-eagle").npu()
+print(model)
+names = [item[0] for item in model.named_parameters()]
+print(names)
