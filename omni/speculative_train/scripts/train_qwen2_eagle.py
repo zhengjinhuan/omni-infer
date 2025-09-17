b90fefa1199276aefe7e9a59215e074f0bc3e269
dingyibin <dingyibin1@huawei.com>
2025-09-17 17:23:20 +0800

    1723

--- omni/speculative_train/scripts/train_qwen2_eagle.py
+++ omni/speculative_train/scripts/train_qwen2_eagle.py
@@ -1,6 +1,6 @@
 from omni.speculative_train.models.auto import AutoDraftModelConfig, AutoEagleDraftModel
 
-config = AutoDraftModelConfig.from_file("/data/model/qwq-32b-eagle")
+config = AutoDraftModelConfig.from_file("/data/model/qwq-32b-eagle/config.json")
 model = AutoEagleDraftModel.from_config(config).npu()
 print(model)
 names = [item[0] for item in model.named_parameters()]
