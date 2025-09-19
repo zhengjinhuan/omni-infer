78cedb527affc73dc079d18594f4fd3a30f54443
jiangyaoguo <jiangyaoguo@outlook.com>
2025-09-19 12:45:53 +0000

    1.修复P侧卡顿 2.更新版本号至0.4.1
    Signed-off-by: jiangyaoguo <jiangyaoguo@outlook.com>

--- omni/adaptors/vllm/worker/npu_model_runner.py
+++ omni/adaptors/vllm/worker/npu_model_runner.py
@@ -582,15 +582,16 @@ class NPUModelRunner(GPUModelRunner):
         for self.curr_step in range(self.total_step):
             start_1 = time.time()
             if not scheduler_output.total_num_scheduled_tokens:
-                if not has_kv_transfer_group():
-                    # Return empty ModelRunnerOuptut if there's no work to do.
-                    return EMPTY_MODEL_RUNNER_OUTPUT
-                res = self.kv_connector_no_forward(scheduler_output)
                 if get_dp_group().world_size > 1:
                    self._dummy_run(1)
                 else:
                     time.sleep(0)
-                return res
+                if not has_kv_transfer_group():
+                    # Return empty ModelRunnerOuptut if there's no work to do.
+                    return EMPTY_MODEL_RUNNER_OUTPUT
+                    
+                return self.kv_connector_no_forward(scheduler_output)
+
             if self.curr_step == 0:
                 attn_metadata, graph_pad_size, sample_indices, positions = self._prepare_inputs(scheduler_output)
             else:
