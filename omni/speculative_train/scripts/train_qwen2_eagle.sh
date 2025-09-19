c34e0c8e6537f2832b1410489109d5c12be05f3b
dingyibin <dingyibin1@huawei.com>
2025-09-19 14:28:49 +0800

    change sh

--- omni/speculative_train/scripts/train_qwen2_eagle.sh
+++ omni/speculative_train/scripts/train_qwen2_eagle.sh
@@ -3,12 +3,12 @@ NUM_GPUS=${1:-16}
 torchrun \
     --standalone \
     --nproc_per_node $NUM_GPUS \
-    -m omni.speculative_train.train_qwen2_eagle \
+    train_qwen2_eagle.py \
     --target-model-path /data/model/QwQ-32B \
     --draft-model-config /data/model/qwq-32b-eagle/config.json \
+    --train-data-path /data/d00646319/offline-dataset/longbench-2048-2048 \
     --train-hidden-states-path /data/d00646319/offline-dataset/longbench-2048-2048 \
     --num-epochs 10 \
-    --batch-size 1 \
     --learning-rate 1e-4 \
     --max-length 2048 \
     --chat-template qwen \
