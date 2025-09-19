b482222e0423f6e44498252a0b12183c512f00fb
dingyibin <dingyibin1@huawei.com>
2025-09-19 14:38:29 +0800

    cuda->npu

--- omni/speculative_train/specforge/distributed.py
+++ omni/speculative_train/specforge/distributed.py
@@ -39,22 +39,22 @@ def init_distributed(timeout: int = 10, tp_size: int = 1):
         tp_size(int): The degree of tensor parallelism
     """
     dist.init_process_group(backend="hccl", timeout=timedelta(minutes=timeout))
-    local_rank = dist.get_rank() % torch.cuda.device_count()
-    torch.cuda.set_device(local_rank)
+    local_rank = dist.get_rank() % torch.npu.device_count()
+    torch.npu.set_device(local_rank)
     print_with_rank(f"bind to device {local_rank}")
 
     world_size = dist.get_world_size()
     dp_size = world_size // tp_size
     assert world_size == tp_size * dp_size, "world size must be divisible by tp size"
     device_mesh = dist.device_mesh.init_device_mesh(
-        "cuda", (dp_size, tp_size), mesh_dim_names=["dp", "tp"]
+        "npu", (dp_size, tp_size), mesh_dim_names=["dp", "tp"]
     )
     print_with_rank(f"device mesh: {device_mesh}")
     tp_group = device_mesh.get_group("tp")
     dp_group = device_mesh.get_group("dp")
 
     # we need to create a 1D submesh
-    tp_device_mesh = dist.DeviceMesh.from_group(tp_group, device_type="cuda")
+    tp_device_mesh = dist.DeviceMesh.from_group(tp_group, device_type="npu")
     global _TP_GROUP, _DP_GROUP, _DEVICE_MESH, _TP_DEVICE_MESH
     _DEVICE_MESH = device_mesh
     _TP_GROUP = tp_group
