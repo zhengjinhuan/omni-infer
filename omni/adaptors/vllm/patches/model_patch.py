1dfae671aae606326ccf6bfcc337cbb125c5c62a
田佳森 <tianjiasen@huawei.com>
2025-09-24 15:28:16 +0800

    bugfix: shm memory issues, using ZMQ communication method

--- omni/adaptors/vllm/patches/model_patch.py
+++ omni/adaptors/vllm/patches/model_patch.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
 import os
 from omni.adaptors.vllm.patches.pangu_patch import patch_pangu
+from omni.adaptors.vllm.patches.shm_bug_fix_patch import patch_shm_to_zmq
+
 
 def patch_vllm_distributed():
     from vllm import distributed
@@ -63,6 +65,7 @@ def patch_all():
     patch_compilation()
     patch_pangu()
     patch_linear()
+    patch_shm_to_zmq()
     _patch_done = True
 
 patch_all() 
