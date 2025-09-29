0f4c23113bd181683313edd59cc6fe514c3b0a80
dingyibin <dingyibin1@huawei.com>
2025-09-29 15:19:11 +0800

    change dataset

--- omni/speculative_train/scripts/train_qwen2_eagle.sh
+++ omni/speculative_train/scripts/train_qwen2_eagle.sh
@@ -8,13 +8,13 @@ torchrun \
     train_offline_eagle.py \
     --target-model-path /data/model/QwQ-32B \
     --draft-model-config /data/model/qwq-32b-eagle/config.json \
-    --train-data-path /data/d00646319/offline-dataset/longbench-2048-2048 \
+    --train-data-path /data/d00646319/offline-dataset/death-no-end_512_1024-2048/hidden-states \
     --num-epochs 10 \
     --learning-rate 1e-4 \
-    --max-length 4096 \
+    --max-length 2048 \
     --chat-template qwen \
     --output-dir ./train-result/ \
-    --ttt-length 2 \
+    --ttt-length 3 \
     --checkpoint /data/model/qwq-32b-eagle \
     --resume \
     --profile --profile-record-shapes \
