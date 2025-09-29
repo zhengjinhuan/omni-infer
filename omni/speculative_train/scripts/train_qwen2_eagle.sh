394f7c26133939f7d233318d0a015b0b6a721e38
dingyibin <dingyibin1@huawei.com>
2025-09-29 11:36:06 +0800

    rename

--- omni/speculative_train/scripts/train_qwen2_eagle.sh
+++ omni/speculative_train/scripts/train_qwen2_eagle.sh
@@ -5,7 +5,7 @@ NUM_GPUS=${1:-16}
 torchrun \
     --standalone \
     --nproc_per_node $NUM_GPUS \
-    train_qwen2_eagle.py \
+    train_offline_eagle.py \
     --target-model-path /data/model/QwQ-32B \
     --draft-model-config /data/model/qwq-32b-eagle/config.json \
     --train-data-path /data/d00646319/offline-dataset/longbench-2048-2048 \
