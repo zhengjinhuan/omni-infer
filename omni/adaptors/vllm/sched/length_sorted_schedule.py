2cd1271062763d46bcbcedfb35122beeaf84216c
武夷山 <wuyishan1@huawei.com>
2025-10-16 20:06:38 +0800

    modified license

--- omni/adaptors/vllm/sched/length_sorted_schedule.py
+++ omni/adaptors/vllm/sched/length_sorted_schedule.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# This file is a part of the vllm-ascend project.
+# Adapted from vllm-project/vllm/vllm/v1/core/sched/scheduler.py
 #
 
 from __future__ import annotations
