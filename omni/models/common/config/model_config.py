578f8ba57fc655d3f85249518263919a93f39eca
chenyj <2361384693@qq.com>
2025-10-17 10:08:38 +0800

    Qwen3 235B preload and scale_parallel

--- omni/models/common/config/model_config.py
+++ omni/models/common/config/model_config.py
@@ -60,6 +60,7 @@ class ModelOperatorOptConfig:
     enable_dsa: bool = False # 使能mla = Indexer + select FA
 
     enable_scale_parallel: bool = False 
+    enable_scale_parallel: bool = False #用于qwen235b的scale_parallel优化启用开关，默认关闭
     
     def __post_init__(self):
         # Check the dependencies of use_omni_placement and omni_placement_config_path
